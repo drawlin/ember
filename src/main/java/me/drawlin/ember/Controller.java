7830e957bfa465362bcf55aeb9a5b4a5599a2e63
drawlin <afx.drawlin@gmail.com>
2020-06-10 10:27:51 -0400

    First commit

---
+++ src/main/java/me/drawlin/ember/Controller.java
@@ -0,0 +1,4 @@
+package me.drawlin.ember;
+
+public class Controller {
+}
---
+++ src/main/java/me/drawlin/ember/Main.java
@@ -0,0 +1,28 @@
+package me.drawlin.ember;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+//        launch(args);
+
+
+    }
+
+}
